a4538463d20a0b2104b7d10ad4d725ff4f512056
Enzo Fokwen <enzo.fokwen-rocher@epitech.eu>
2024-04-03 11:02:39 +0200

    [ADD] function to prepare map for algo to run a second time

---
+++ src/opti_algo.c
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2024
+** AMAZED
+** File description:
+** Functions related to opti for the algo
+*/
+
+#include <stdbool.h>
+
+static void reset_all_node(linked_list_t *nodes)
+{
+    while (nodes != NULL) {
+        nodes->node->checked = false;
+        nodes = nodes->next;
+    }
+}
+
+static void set_path_as_visited(linked_list_t *list)
+{
+    while (list != NULL) {
+        list->node->visited = true;
+        list = list->next;
+    }
+}
+
+void set_map_to_find_new_path(linked_list_t *nodes, linked_list_t **paths)
+{
+    reset_all_node(nodes);
+    for (unsigned int i = 0; paths[i] != NULL; i++)
+        set_path_as_visited(paths[i]);
+}
