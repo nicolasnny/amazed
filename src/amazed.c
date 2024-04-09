6047c6e66c574834424d56b2e3dab21827e278b2
Enzo Fokwen <enzo.fokwen-rocher@epitech.eu>
2024-04-09 11:28:29 +0200

    fix: not displaying the output if it doesn't exist

--- src/amazed.c
+++ src/amazed.c
@@ -58,6 +58,7 @@ int amazed(void)
     connections = get_connections(data);
     link_array = init_map(connections, rooms);
     path_list = get_path_list(data, rooms, link_array);
-    print_basic_output(rooms, data, connections, path_list);
+    if (path_list != NULL)
+        print_basic_output(rooms, data, connections, path_list);
     return SUCCESS;
 }
