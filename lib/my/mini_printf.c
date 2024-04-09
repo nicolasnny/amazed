f25949e3c0e40e064fa5db1aafc9b1c5d4b1d80a
damsidams <damien.collet@epitech.eu>
2024-04-09 11:59:47 +0200

    [FIX] compilation ERROR

--- lib/my/mini_printf.c
+++ lib/my/mini_printf.c
@@ -9,33 +9,6 @@
 #include <unistd.h>
 #include "my.h"
 
-static int print_neg(int nb)
-{
-    if (nb < 0) {
-        my_putchar('-');
-        nb *= -1;
-    }
-    return nb;
-}
-
-static int print_nb(int nb)
-{
-    char res;
-
-    if (nb / 10 > 0)
-        print_nb(nb / 10);
-    res = (nb % 10) + '0';
-    my_putchar(res);
-    return 0;
-}
-
-static int my_put_nbr(int nb)
-{
-    nb = print_neg(nb);
-    print_nb(nb);
-    return 0;
-}
-
 static int is_var_good(char c)
 {
     if (c == 'i' || c == 'd')
