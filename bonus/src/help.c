d8d516b273fed44a3fee3fd0a7a4b44ed45c248e
Enzo Fokwen <enzo.fokwen-rocher@epitech.eu>
2024-04-12 10:18:16 +0200

    [ADD] display more content in the -h flag

--- bonus/src/help.c
+++ bonus/src/help.c
@@ -20,10 +20,18 @@ static void disp_logo(void)
     mini_printf(" ##  ##   ##   ##  ##  ##   #######  #######  #####\n");
     mini_printf("-*------*-----*--------*--*---*-----*-----*---*------\n");
     mini_printf("---*--------*-----*-----*-----*-------*--*--------*--\n");
+    my_putchar('\n');
 }
 
 int disp_help(void)
 {
     disp_logo();
+    mini_printf("SYNOPSIS\n\t./amazed [OPTION]\n\n");
+    mini_printf("DESCRIPTION\n");
+    mini_printf("\tMove all the robots in a maze from the entrance to the ");
+    mini_printf("exit as fast as possible. The maze and robots configuration ");
+    mini_printf("are given in the standard input.\n\n");
+    mini_printf("OPTIONS\n\t-h\tdisplay help\n");
+    mini_printf("\t-d\tdisplay robots state live with ncurse\n");
     return 0;
 }
