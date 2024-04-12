85869d031275e6678724e61ebf89f5dcc5c003c0
Enzo Fokwen <enzo.fokwen-rocher@epitech.eu>
2024-04-12 11:58:02 +0200

    [ADD] main

---
+++ bonus/src/main.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPE-200-PAR-2-1-amazed-enzo.fokwen-rocher
+** File description:
+** main.c
+*/
+
+#include "amazed.h"
+#include "my.h"
+
+int main(int argc, char **argv)
+{
+    if (argc == 2 && my_strcmp(argv[1], "-h") == 0)
+        return disp_help();
+    if ((argc == 2 || argc == 3) && my_strcmp(argv[1], "-d") == 0)
+        return ncurse_amazed(argc, argv);
+    if (argc != 1)
+        return ERROR;
+    return amazed();
+}
